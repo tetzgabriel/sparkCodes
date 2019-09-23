dd83ee683fd688941452d876dfbca4e993a394d9
Tetzlaf <tetz.gabriel@gmail.com>
2019-09-23 13:21:07 -0300

    Finished MissingData.scala file

--- MissingData.scala
+++ MissingData.scala
@@ -10,3 +10,23 @@ df.printSchema()
 println("")
 println("Printing DataFrame")
 df.show()
+
+println("")
+println("Removing rows that contains null values")
+df.na.drop().show()
+
+println("")
+println("Removing rows that not contains at least two non-null values")
+df.na.drop(2).show()
+
+println("")
+println("Filling integer null values with 100")
+df.na.fill(100).show()
+
+println("")
+println("Filling string null values with Missing")
+df.na.fill("Missing").show()
+
+println("")
+println("Filling Name column null values with Missing")
+df.na.fill("Missing", Array("Name")).show()
