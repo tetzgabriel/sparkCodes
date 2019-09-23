265bffb29578beec41692359f6c6c355a864d7e1
Tetzlaf <tetz.gabriel@gmail.com>
2019-09-23 09:29:20 -0300

    Counting and correlations

--- DataFrameOperations.scala
+++ DataFrameOperations.scala
@@ -15,3 +15,20 @@ df.filter("Close > 480").show() //df.filter($"Close" > 480).show() -> Same Resul
 println("")
 println("Using multiple filters")
 df.filter($"Close"< 480 && $"High" < 480).show() //df.filter("Close < 480 AND High < 480") -> Same Result but SQL notation
+
+println("")
+println("Filtering with equal")
+df.filter($"High" === 484.40).show() //df.filter("High = 484.40").show() -> Same Result but SQL notation
+
+println("")
+println("Saving results in a Scala object")
+val CH_low = df.filter($"Close"< 480 && $"High" < 480).collect()
+
+println("")
+println("Counting results")
+val lowCount = df.filter($"Close"< 480 && $"High" < 480).count()
+
+println("")
+println("Correlations")
+df.select(corr("High","Low")).show()
+
