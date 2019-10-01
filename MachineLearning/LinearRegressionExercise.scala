d857524ba4901cd2473d2e42176524f68e23e048
Tetzlaf <tetz.gabriel@gmail.com>
2019-10-01 05:43:24 -0300

    Added logs for LinearRegressionExercise.scala

--- MachineLearning/LinearRegressionExercise.scala
+++ MachineLearning/LinearRegressionExercise.scala
@@ -6,12 +6,20 @@ import org.apache.spark.ml.linalg.Vectors
 
 Logger.getLogger("org").setLevel(Level.ERROR)
 
+println("")
+println("Creating spark session")
 val spark = SparkSession.builder().getOrCreate()
-// Use Spark to read in the Ecommerce Customers csv file.
+
+println("")
+println("Reading file")
 val data = spark.read.option("header","true").option("inferSchema","true").format("csv").load("../Data/Clean-Ecommerce.csv")
-// Print the Schema of the DataFrame
+
+println("")
+println("Printing schema")
 data.printSchema()
 
+println("")
+println("Print example row")
 val columnNames = data.columns
 val firstRow = data.head(1)(0)
 for(index <- Range(1, columnNames.length)){
@@ -23,6 +31,8 @@ for(index <- Range(1, columnNames.length)){
 // Rename the Yearly Amount Spent Column as "label"
 // Also grab only the numerical columns from the data
 // Set all of this as a new dataframe called df
+println("")
+println("Putting in label/features model")
 val df = (data.select(data("Yearly Amount Spent").as("label"),
                       $"Avg Session Length", $"Time on App",
                       $"Time on Website", $"Length of Membership"))
@@ -33,8 +43,16 @@ val assembler = (new VectorAssembler().setInputCols(Array("Avg Session Length", 
 val output = assembler.transform(df).select($"label",$"features")
 output.show()
 
+println("")
+println("Creating linear regression object")
 val lr = new LinearRegression()
+
+println("")
+println("Fitting model")
 val lrModel = lr.fit(output)
+
+println("")
+println("Summaring the results")
 val trainingSummary = lrModel.summary
 
 println(s"Coefficients: ${lrModel.coefficients} Intercept: ${lrModel.intercept}")
